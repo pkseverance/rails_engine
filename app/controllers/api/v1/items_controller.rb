b93980814973c77381d8735cf1473ae6c158f48c
Patricia Severance <patriciakseverance@gmail.com>
2023-02-08 16:26:58 -0700

    Implement create and delete functionality for items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -11,4 +11,31 @@ class Api::V1::ItemsController < ApplicationController
             status: 404
         end
     end
+
+    def create
+        if Item.create(item_params).valid?
+            render json: ItemSerializer.new(Item.create(item_params)),
+            status: 201
+        else
+            render status: 422
+        end
+    end
+
+    def destroy
+        @item = Item.find(params[:id])
+        destroy_invoice_items
+        @item.destroy
+    end
+
+    private
+    
+    def item_params
+        params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+    end
+
+    def destroy_invoice_items
+        @item.invoice_items.each do |invoice_item|
+            invoice_item.destroy if invoice_item.item == @item
+        end
+    end
 end
