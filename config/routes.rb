803968c390de001ffd3006b1ca4b480ead65c048
Patricia Severance <patriciakseverance@gmail.com>
2023-02-08 12:11:18 -0700

    Remove whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-
       resources :items, only: [:index, :show] do
           resource :merchant, only: [:show]
       end
