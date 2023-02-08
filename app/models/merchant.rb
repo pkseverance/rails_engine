c5513c973bfa02c9507304e1deba3532faba1c6e
Patricia Severance <patriciakseverance@gmail.com>
2023-02-08 16:24:56 -0700

    Add more models and associations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,8 @@
 class Merchant < ApplicationRecord
     has_many :items
+    has_many :invoice_items, through: :items
+    has_many :invoices, through: :invoice_items
+    has_many :customers, through: :invoices
+    has_many :transactions, through: :invoices
+    has_many :discounts
 end
