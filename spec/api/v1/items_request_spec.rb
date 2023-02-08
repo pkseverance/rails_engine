961d820beaf4188153226c693950dde9533393e8
Patricia Severance <patriciakseverance@gmail.com>
2023-02-08 09:11:18 -0700

    add items_request_spec(WIP)

---
+++ spec/api/v1/items_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Items Endpoint' do
+    describe 'Items Index' do
+        it 'returns a JSON response containing a list of items and their attributes' do
+            
+        end
+    end
+
+    describe 'Item Show' do
+        it "returns a JSON response containing one item and it's attributes" do
+            
+        end
+    end
+end
