99523fe9b37b36e5751f679ffacba4575269af1b
Patricia Severance <patriciakseverance@gmail.com>
2023-02-08 10:37:11 -0700

    Implement show action in items_controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,10 +4,10 @@ class Api::V1::ItemsController < ApplicationController
     end
 
     def show
-
+        render json: ItemSerializer.new(Item.find(params[:id]))
     end
 
     def merchant
-        
+
     end
 end
