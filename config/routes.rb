4eeb65ba02365febb9c2a69c53884caf95a87271
Patricia Severance <patriciakseverance@gmail.com>
2023-02-08 12:45:06 -0700

    Add create route to /api/v1/items

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show] do
+      resources :items, only: [:index, :show, :create] do
           resource :merchant, only: [:show]
       end
 
