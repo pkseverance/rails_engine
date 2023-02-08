b8e01a1eaff9171f32f4fe53dbc23333b440ad35
Patricia Severance <patriciakseverance@gmail.com>
2023-02-08 10:08:17 -0700

    Add render method to index action in items_controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::ItemsController < ApplicationController
     def index
-        render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
+        render json: ItemSerializer.new(Item.all)
+    end
+
+    def show
+
+    end
+
+    def merchant
+        
     end
 end
