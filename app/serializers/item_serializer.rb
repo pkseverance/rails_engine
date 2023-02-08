15c5528a7b1c142cd4fd1d25766fdf44519f7953
Patricia Severance <patriciakseverance@gmail.com>
2023-02-08 12:12:21 -0700

    Add not_found instance method to item_serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,14 @@
 class ItemSerializer
   include JSONAPI::Serializer
   attributes :name, :description, :unit_price, :merchant_id
+
+  def self.not_found
+    {
+      "data": 
+      {
+          "id": "nil",
+          "attributes": []
+      }
+    }
+  end
 end
